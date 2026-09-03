62815a64e71850dd45b8e935fbbc1ee254ce3ab1
saya1805 <sayalishelke000@gmail.com>
2026-09-03 18:43:15 +0530

    add all css and html changes for sale

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { UserAuthComponent } from './Auth/user-auth/user-auth.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SaleComponent } from './sidebar/sale/sale.component';
 
 export const routes: Routes = [
     {
@@ -20,5 +21,9 @@ export const routes: Routes = [
     {
         path:'dashboard',
         component:DashboardComponent
+    },
+    {
+        path:'sale',
+        component:SaleComponent
     }
 ];
---
+++ src/app/sidebar/sale/sale.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sale',
+  imports: [],
+  templateUrl: './sale.component.html',
+  styleUrl: './sale.component.css',
+})
+export class SaleComponent {
+
+}
